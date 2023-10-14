32d0abedf12fce8b575e13dc3694c0a77766c6b1
Bibizareen <142078516+Bibizareen@users.noreply.github.com>
2023-10-14 09:59:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// data types
+// "hello world"  string
+// 1,2,3,123  integer
+// 1.3 float
+
+
+
+
+
+
+// alert("hello  world")
+
+// document.write(" <h1>it is js first class </h1>")
+
+// document.write("<h1>" + "hello world" +"</h1>")
+
+// var firstName= "hello"
+// var lastName= "world"
+// document.write(firstName+" "+lastName)
+let language = "JavaScript";
+let message = `Let's learn ${language}`;
+console.log(mess.age);
+// var num1="y2" string
+// var num2= 5    integar
+// document.write(num1 * num2)
+// + sign pe concart krega os surat me jb string hogi
+// + sign add krega jb dono integer  hogi
+// - sub krega jb ak string or ak integer ho tb bh or jb dono integer ho tb bh 
+// same - wale chez Mult div mod pe apply hogi 
+// jb ak string ho usme alphabet ho or ak int ho or min mult div lga hoga to run time me decide krega or operation perform krdega
+
+
+var a = prompt("Enter Value 1")
+document.write(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
